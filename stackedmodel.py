0326cb5df648aa8ca146fb64d2ede63207077087
Lukas Kosina <lukas@kosinovi.eu>
2019-06-01 19:20:28 +0200

    Add fixed models

--- stackedmodel.py
+++ stackedmodel.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from xgboost import XGBClassifier
 import numpy as np
-import numpy.testing as npt
+
 
 class StackedModel:
 	def __init__(self, y_acc, y_type):
@@ -17,9 +17,6 @@ class StackedModel:
 		self.linearLogReg = LogisticRegression(multi_class="multinomial", C=1.0, penalty="l2", solver="saga")
 
 	def fit(self, X, y_acc, y_type):
-		npt.assert_array_equal(np.unique(y_acc), [0, 1])
-		npt.assert_array_equal(np.unique(y_type), [0, 1, 2, 3])
-
 		y_compound = make_single_number(y_type, y_acc)
 		self.typeXG.fit(X, y_type)
 		self.accXG.fit(X, y_acc)
@@ -37,11 +34,26 @@ class StackedModel:
 		)
 
 
+def array_contains(input_array, check_array):
+	return all([i in check_array for i in input_array])
+
+
+def convert_to_numpy(input_obj):
+	if isinstance(input_obj, np.ndarray):
+		return input_obj
+	else:
+		return np.array(input_obj)
+
+
 def make_single_number(y_type_input, y_acc_input):
-	return y_type_input + (10 * y_acc_input)
+	array_contains(y_acc_input, [0, 1])
+	array_contains(y_type_input, [0, 1, 2, 3])
+
+	return convert_to_numpy(y_type_input) + (10 * convert_to_numpy(y_acc_input))
 
 
 def make_multiple_categories(y_compound):
+	y_compound = int(y_compound)
 	acc_idx = y_compound // 10
 	tram_type_idx = y_compound % 10
 	return acc_idx, tram_type_idx
